f7b935a203fa87ab1875a918997508a84112461c
ZHICHAO PENG <zhichaopeng@ZHICHAOdeMacBook-Pro.local>
2019-07-12 14:33:40 -0600

    f(u), linear integrator defined, considering how to link with other code

---
+++ miniapps/dpg_grad_shafranov/RHSCoefficient.hpp
@@ -0,0 +1,45 @@
+#include "mfem.hpp"
+
+using namespace std;
+
+namespace mfem
+{
+	class RHSCoefficient : public Coefficient
+	{
+		private:
+			GridFunction *u;
+		public:
+			RHSCoefficient(GridFunction *_u)
+			{ 
+				u = _u;
+			}
+
+			double Eval(ElementTransformation &T, const IntegrationPoint &ip);
+	
+	};
+
+	double RHSCoefficient::Eval(ElementTransformation &T, const IntegrationPoint &ip)
+	{
+		/* parameters */
+		double kr  = 1.15*M_PI, kz = 1.15;
+		double r0  = -0.5;
+
+
+		/* coordinate */
+		double x[3];
+		Vector transip(x,3);
+		T.Transform(ip, transip);
+
+		double xi = transip(0);
+		double yi = transip(1);
+
+		double uip = u->GetValue(T.ElementNo,ip);
+
+		return uip;
+		return  uip - xi * xi * (sin(4*M_PI*xi) + sin(4*M_PI*yi) + yi )
+				-12. *M_PI * cos(4.*M_PI * xi) 
+		        +xi * 16. *M_PI*M_PI * sin(4.*M_PI * xi)
+				+xi * 16. *M_PI*M_PI * sin(4.*M_PI * yi);
+	}
+
+}
